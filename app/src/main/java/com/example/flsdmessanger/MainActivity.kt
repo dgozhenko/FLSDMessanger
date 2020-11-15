45f983c761c68701b3adee7d33c8dbeba13cf1ac
Daniil Hozhenko <daniil.gozhenko@gmail.com>
2020-11-15 19:38:37 +0200

    Create Registration template

--- app/src/main/java/com/example/flsdmessanger/MainActivity.kt
+++ app/src/main/java/com/example/flsdmessanger/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.flsdmessanger
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        register_button.setOnClickListener {
+            val email = email_edit_register.text.toString()
+            val password = password_edit_register.text.toString()
+
+            Log.d("MainActivity", "Email: $email")
+            Log.d("MainActivity", "Password: $password")
+        }
     }
 }
