a744c53f77d49c84ff521ffe0c0aa303ffd798f7
Daniil Hozhenko <daniil.gozhenko@gmail.com>
2020-11-18 18:25:43 +0200

    Add divider and change xml files

--- app/src/main/java/com/example/flsdmessanger/ui/new_message/NewMessageFragment.kt
+++ app/src/main/java/com/example/flsdmessanger/ui/new_message/NewMessageFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.flsdmessanger.databinding.NewMessageFragmentBinding
 import kotlinx.android.synthetic.main.new_message_fragment.*
@@ -24,6 +25,7 @@ class NewMessageFragment: Fragment() {
         val adapter = NewMessageAdapter()
         recycler_view_new_message.layoutManager = LinearLayoutManager(requireContext())
         recycler_view_new_message.adapter = adapter
+        recycler_view_new_message.addItemDecoration(DividerItemDecoration(recycler_view_new_message.context, DividerItemDecoration.VERTICAL))
         newMessageViewModel.users.observe(viewLifecycleOwner, {
             adapter.setUser(it)
         })
