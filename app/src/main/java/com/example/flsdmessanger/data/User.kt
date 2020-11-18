5aed458165cd7d0f247f2b3bd5b4e6d60259d65c
Daniil Hozhenko <daniil.gozhenko@gmail.com>
2020-11-18 20:45:38 +0200

    Parcelize User data

--- app/src/main/java/com/example/flsdmessanger/data/User.kt
+++ app/src/main/java/com/example/flsdmessanger/data/User.kt
@@ -1,5 +1,9 @@
 package com.example.flsdmessanger.data
 
-class User(val uid: String, val username: String, val profileImage: String) {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+class User(val uid: String, val username: String, val profileImage: String) : Parcelable {
     constructor() : this("", "", "")
 }
